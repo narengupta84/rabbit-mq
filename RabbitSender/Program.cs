a8b91f9937f47b0675d5261263aec9ef8fc11dbb
Narendra <narengupta84@gmail.com>
2023-05-12 16:00:28 +0700

    feat: :sparkles: Send message to Rabbit MQ

--- RabbitSender/Program.cs
+++ RabbitSender/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new Uri("amqp://admin:AT63Ukv4@rabbitmq-125957-0.cloudclusters.net:10064");
+factory.ClientProvidedName = "Rabbit Sender App";
+
+IConnection cnn = factory.CreateConnection();
+
+IModel channel = cnn.CreateModel();
+
+string exchangeName = "DemoExchange";
+string routingKey = "demo-routing-key";
+string queueName = "DemoQueue";
+
+channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+channel.QueueDeclare(queueName, false, false, false, null);
+channel.QueueBind(queueName, exchangeName, routingKey, null);
+
+byte[] messageBodyBytes = Encoding.UTF8.GetBytes("Hello World!");
+channel.BasicPublish(exchangeName, routingKey, null, messageBodyBytes);
+
+channel.Close();
+cnn.Close();
