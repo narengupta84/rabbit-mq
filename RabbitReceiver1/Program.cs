9da6baa07c5fcb3c00710abb8ce04a755dc1b725
Narendra <narengupta84@gmail.com>
2023-05-12 16:26:49 +0700

    feat: :sparkles: Receiver1 created

---
+++ RabbitReceiver1/Program.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new Uri("amqp://admin:AT63Ukv4@rabbitmq-125957-0.cloudclusters.net:10064");
+factory.ClientProvidedName = "Rabbit Receiver1 App";
+
+IConnection cnn = factory.CreateConnection();
+
+IModel channel = cnn.CreateModel();
+
+string exchangeName = "DemoExchange";
+string routingKey = "demo-routing-key";
+string queueName = "DemoQueue";
+
+channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+channel.QueueDeclare(queueName, false, false, false, null);
+channel.QueueBind(queueName, exchangeName, routingKey, null);
+channel.BasicQos(0, 1, false);
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (sender, args) =>
+{
+    var body = args.Body.ToArray();
+
+    string messgae = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Message Received :{messgae}");
+
+    //Send message to DB
+
+    channel.BasicAck(args.DeliveryTag, false);
+};
+
+string consumerTag = channel.BasicConsume(queueName, false, consumer);
+Console.ReadLine();
+channel.BasicCancel(consumerTag);
+
+channel.Close();
+cnn.Close();
